c3d32e5069d4b4176dec2b2014f45f2e73307eb6
alexander ames <alexanderthomasames@gmail.com>
2019-06-09 17:01:26 -0600

    works crud

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -2,4 +2,37 @@ class WorksController < ApplicationController
   def index
     @works = Work.all
   end
+
+  def new
+    @works = Work.new
+  end
+
+  def create
+    @works = Work.new(params.require(:work).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @works.save
+        format.html { redirect_to works_path, notice: 'works was successfully created.' }
+      else
+        format.html { render :new }
+
+      end
+    end
+  end
+
+  def edit
+    @works = Work.find(params[:id])
+  end
+
+  def update
+    @works = Work.find(params[:id])
+
+    respond_to do |format|
+      if @works.update(params.require(:work).permit(:title, :subtitle, :body))
+        format.html { redirect_to works_path, notice: 'Blog was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
 end
